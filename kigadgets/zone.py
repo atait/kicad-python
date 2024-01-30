cf83f4d90eb3abe848e87fdd36daf3f077fd540f
Alex Tait <atait@ieee.org>
2024-01-30 15:28:45 -0500

    Overhaul Zone layerset and allow for more reasonable allow access

--- kigadgets/zone.py
+++ kigadgets/zone.py
@@ -6,48 +6,44 @@ from kigadgets.item import HasConnection, HasLayer, Selectable, BoardItem
 from kigadgets.layer import LayerSet
 
 class KeepoutAllowance(object):
-    """ Gives key-value interface of the form
-            my_zone.is_keepout = True
-            my_zone.allow['tracks'] = False
+    """ Gives key-value and dot interfaces of the form
+            zz.is_keepout = True
+            zz.allow['tracks'] = False
+            print(my_zone.allow.tracks)
     """
     def __init__(self, zone):
-        self._zone = zone
+        super().__setattr__('_zone', zone)
+        # self._zone = zone
 
-    def __getitem__(self, item):
-        # if not self._zone.is_keepout:
-        #     print('Warning: '
-        #         'Keepout settings do not apply to fill zones.'
-        #         ' Call "my_zone.is_keepout = True" first.'
-        #     )
-        if item == 'tracks':
-            return not self._zone._obj.GetDoNotAllowTracks()
-        elif item == 'pour':
-            return not self._zone._obj.GetDoNotAllowCopperPour()
-        elif item == 'vias':
-            return not self._zone._obj.GetDoNotAllowVias()
-        else:
-            raise KeyError(
-                'Invalid zone keepout allowance type: {}. '
-                'Allowed types are "tracks, pour, vias"'.format(item)
-            )
+    @property
+    def tracks(self):
+        return not self._zone._obj.GetDoNotAllowTracks()
 
-    def __setitem__(self, item, value):
-        # if not self._zone.is_keepout:
-        #     print('Warning: '
-        #         'Keepout settings do not apply to fill zones.'
-        #         ' Call "my_zone.is_keepout = True" first.'
-        #     )
-        if item == 'tracks':
-            self._zone._obj.SetDoNotAllowTracks(not bool(value))
-        elif item == 'pour':
-            self._zone._obj.SetDoNotAllowCopperPour(not bool(value))
-        elif item == 'vias':
-            self._zone._obj.SetDoNotAllowVias(not bool(value))
-        else:
-            raise KeyError(
-                'Invalid zone keepout allowance type: {}. '
-                'Allowed types are "tracks, pour, vias"'.format(item)
-            )
+    @tracks.setter
+    def tracks(self, value):
+        self._zone._obj.SetDoNotAllowTracks(not bool(value))
+
+    @property
+    def pour(self):
+        return not self._zone._obj.GetDoNotAllowCopperPour()
+
+    @pour.setter
+    def pour(self, value):
+        self._zone._obj.SetDoNotAllowCopperPour(not bool(value))
+
+    @property
+    def vias(self):
+        return not self._zone._obj.GetDoNotAllowVias()
+
+    @vias.setter
+    def vias(self, value):
+        self._zone._obj.SetDoNotAllowVias(not bool(value))
+
+    def __getitem__(self, attr):
+        return getattr(self, attr)
+
+    def __setitem__(self, attr, value):
+        setattr(self, attr, value)
 
     def __str__(self):
         return str({k: self[k] for k in ['tracks', 'pour', 'vias']})
@@ -56,13 +52,13 @@ class KeepoutAllowance(object):
         return type(self).__name__ + str(self)
 
 
-class Zone(HasConnection, HasLayer, Selectable, BoardItem):
+class Zone(HasConnection, Selectable, BoardItem):
     _wraps_native_cls = SWIGtype.Zone
 
     def __init__(self, layer='F.Cu', board=None):
         self._obj = SWIGtype.Zone(board and board.native_obj)
-        self.layer = layer
         raise NotImplementedError('Constructor not supported yet')
+        self.layer = layer
 
     @property
     def clearance(self):
@@ -124,6 +120,21 @@ class Zone(HasConnection, HasLayer, Selectable, BoardItem):
     @layerset.setter
     def layerset(self, new_lset):
         self._obj.SetLayerSet(new_lset._obj)
+
+    @property
+    def layer(self):
+        raise RuntimeError(
+            'Zone does not have a valid layer because there might be multiple layers. '
+            'Use "zone.layers" property instead for lists of strings, '
+            'or use "zone.layerset" to interact with LayerSet.add_layer and LayerSet.remove_layer')
+
+    @property
+    def layers(self):
+        return self.layerset.layers
+
+    @layers.setter
+    def layers(self, new_lylist):
+        self.layerset.layers = new_lylist
 
     def geohash(self):
         hash((
