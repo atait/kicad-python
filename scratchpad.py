915a39a7a71fb0e383c3807a1af3f09889037d4f
Matthew Wildoer <mawildoer@gmail.com>
2024-01-31 12:18:03 -0800

    Tidy scratch pads

--- scratchpad.py
+++ scratchpad.py
@@ -1,53 +1,27 @@
-# %%
+#%%
 import Pyro5.api
 import Pyro5.errors
-import sys
 
 from typing import TYPE_CHECKING
+import Pyro5.api
+
 
 if TYPE_CHECKING:
     from kigadgets.board import Board
-
-# %%
-import Pyro5.api
 
 
 uri = "PYRONAME:kigadgets.Board"
 
 board = Pyro5.api.Proxy(uri)
 
-# # %%
-# try:
-#     for results in dummy.thingos:
-#         print(results)
-# except Exception:
-#     print("Pyro traceback:")
-#     print("".join(Pyro5.errors.get_pyro_traceback()))
-
-# # %%
-# try:
-#     b: "Board" = board.from_editor()
-#     for via in b.vias:
-#         print(via.drill)
-#     # print(b.filename)
-# except Exception:
-#     print("Pyro traceback:")
-#     print("".join(Pyro5.errors.get_pyro_traceback()))
-
 # %%
+# The fun things!
+
 b: "Board" = board.from_editor()
 for i in range(10):
     b.add_via((50, 50 + i * 5))
 
-# # %%
-# len(list(b.vias))
-# # %%
+#%%
 
-# b: "Board" = board.from_editor()
-# b.add_via((101, 50))
-
-# %%
 with Pyro5.api.Proxy("PYRONAME:kigadgets.Pcbnew") as pcbnew:
     pcbnew.refresh()
-
-# %%
